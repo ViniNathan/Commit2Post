d019726b3edd5b1301597b08794b11e3bf16b949
ViniNathan <viniciosnathan@gmail.com>
2026-02-02 18:59:03 -0300

    refactor: simplify Header component structure and update UI elements

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,33 +1,17 @@
-"use client";
-import Link from "next/link";
+import React from 'react'
 
-import { ModeToggle } from "./mode-toggle";
-import UserMenu from "./user-menu";
-
-export default function Header() {
-  const links = [
-    { to: "/", label: "Home" },
-    { to: "/dashboard", label: "Dashboard" },
-  ] as const;
-
+const Header = () => {
   return (
-    <div>
-      <div className="flex flex-row items-center justify-between px-2 py-1">
-        <nav className="flex gap-4 text-lg">
-          {links.map(({ to, label }) => {
-            return (
-              <Link key={to} href={to}>
-                {label}
-              </Link>
-            );
-          })}
-        </nav>
-        <div className="flex items-center gap-2">
-          <ModeToggle />
-          <UserMenu />
-        </div>
+    <div className="p-4 flex flex-row w-full justify-between items-center">
+      <div className="text-2xl font-bold tracking-tight">
+          Commit2Post <span className="text-amber-300">.</span>
       </div>
-      <hr />
+      <button className=" cursor-pointer border-2 border-amber-300 text-amber-300 px-6 py-2 rounded-full hover:bg-amber-300 hover:text-black transition-all duration-300 font-semibold">
+        Viralizar
+      </button>
     </div>
-  );
+
+  )
 }
+
+export default Header
